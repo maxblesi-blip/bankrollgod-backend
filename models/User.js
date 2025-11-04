6b45f18e542bfe90dd560b95614fb34a906bbd54
maxblesi-blip <maxblesi@gmail.com>
2025-11-04 14:26:26 +0100

    fix: Convert User model from Mongoose to Sequelize

--- models/User.js
+++ models/User.js
@@ -1,15 +1,89 @@
-// models/User.js - erweitert um Profile-Settings
-const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  first_name: { type: String },
-  last_name: { type: String },
-  nickname: { type: String },                    // ← NEU für Display-Name
-  profilePublic: { type: Boolean, default: false },     // ← NEU
-  showStats: { type: Boolean, default: true },          // ← NEU
-  allowMessages: { type: Boolean, default: true },      // ← NEU
-  dataSharing: { type: Boolean, default: false },       // ← NEU
-  created_at: { type: Date, default: Date.now },
-  updated_at: { type: Date, default: Date.now }
+// models/User.js
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/database');
+const bcrypt = require('bcryptjs');
+
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true
+  },
+  username: {
+    type: DataTypes.STRING(50),
+    allowNull: false,
+    unique: true,
+    validate: {
+      len: [3, 50]
+    }
+  },
+  email: {
+    type: DataTypes.STRING(255),
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true
+    }
+  },
+  password: {
+    type: DataTypes.STRING(255),
+    allowNull: false
+  },
+  first_name: {
+    type: DataTypes.STRING(100),
+    allowNull: true
+  },
+  last_name: {
+    type: DataTypes.STRING(100),
+    allowNull: true
+  },
+  nickname: {
+    type: DataTypes.STRING(50),
+    allowNull: true,
+    comment: 'Display name for profile'
+  },
+  profile_public: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false
+  },
+  show_stats: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: true
+  },
+  allow_messages: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: true
+  },
+  data_sharing: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false
+  }
+}, {
+  tableName: 'users',
+  timestamps: true,
+  underscored: true,
+  hooks: {
+    beforeCreate: async (user) => {
+      if (user.password) {
+        user.password = await bcrypt.hash(user.password, 10);
+      }
+    },
+    beforeUpdate: async (user) => {
+      if (user.changed('password')) {
+        user.password = await bcrypt.hash(user.password, 10);
+      }
+    }
+  }
 });
+
+// Instance Methods
+User.prototype.validatePassword = async function(password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+User.prototype.toSafeObject = function() {
+  const { password, ...safeUser } = this.toJSON();
+  return safeUser;
+};
+
+module.exports = User;
